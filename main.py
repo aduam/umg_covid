4ff27b6407257b5e1a88bee7456c0744f331b5c9
aduam <alanduam@gmail.com>
2020-06-05 02:41:00 -0600

    view, and connection done

--- main.py
+++ main.py
@@ -3,25 +3,104 @@ from pyswip.prolog import Prolog
 
 app = Flask(__name__)
 
-def prol():
-  prolog = Prolog()
-  prolog.assertz('father(michael,john)')
-  prolog.assertz("father(michael,gina)")
-  list(prolog.query("father(michael,X)")) == [{'X': 'john'}, {'X': 'gina'}]
-  for soln in prolog.query("father(X,Y)"):
-      print(soln["X"], "is the father of", soln["Y"])
+def prol(prolog):
+  prolo = Prolog()
+  prolo.consult("proyecto_final.pl")
+  peso = prolog['peso']
+  fiebre = prolog['fiebre']
+  fatiga = prolog['fatiga']
+  tos_seca = prolog['tos_seca']
+  falta_apetito = prolog['falta_apetito']
+  dolor_cuerpo = prolog['dolor_cuerpo']
+  dificultad_respirar = prolog['dificultad_respirar']
+  mucosidad = prolog['mucosidad']
 
-@app.route('/home')
+  h = int(fiebre) * 20
+  i = int(fatiga) * 20
+  j = int(tos_seca) * 20
+  k = int(falta_apetito) * 10
+  l = int(dolor_cuerpo) * 10
+  m = int(dificultad_respirar) * 10
+  n = int(mucosidad) * 3
+  r = h + i + j + k + l + m + n
+
+  result = {
+    'total_covid': r,
+    'fiebre': {
+      'titulo': 'Fiebre',
+      'medicina': [
+        { 'titulo': 'Acetaminofén', 'precio': 5.00 },
+        { 'titulo': 'Ibuprofeno', 'precio': 10.50 },
+        { 'titulo': 'Aspirina', 'precio': 8.15 },
+      ],
+    } if fiebre == '1' else False,
+    'fatiga': {
+      'titulo': 'Fatiga',
+      'medicina': [
+        { 'titulo': 'Cafeína', 'precio': 15.00 },
+        { 'titulo': 'Metilfenidato', 'precio': 18.50 },
+        { 'titulo': 'Dextroafetamina', 'precio': 30.54 },
+        { 'titulo': 'Modafinilo', 'precio': 21.75 },
+      ],
+    } if fatiga == '1' else False,
+    'tos_seca': {
+      'titulo': 'Tos seca',
+      'medicina': [
+        { 'titulo': 'Levodropropizina', 'precio': 40.41 },
+        { 'titulo': 'Dropropizina', 'precio': 50.50 },
+        { 'titulo': 'Dextrometorfano', 'precio': 75.00 },
+        { 'titulo': 'Cloval', 'precio': 65.00 },
+      ],
+    } if tos_seca == '1' else False,
+    'falta_apetito': {
+      'titulo': 'Falta de apetito',
+      'medicina': [
+        { 'titulo': 'Aceite de pescado', 'precio': 150 },
+        { 'titulo': 'Iproheptadina', 'precio': 109.50 },
+        { 'titulo': 'Pizotifeno', 'precio': 85.50 },
+      ],
+    } if falta_apetito == '1' else False,
+    'dolor_cuerpo': {
+      'titulo': 'Dolor de cuerpo',
+      'medicina': [
+        { 'titulo': 'Ibuprofeno', 'precio': 10.50 },
+        { 'titulo': 'Paracetamol', 'precio': 35.60 },
+        { 'titulo': 'Dipirona', 'precio': 40.45 },
+      ],
+    } if dolor_cuerpo == '1' else False,
+    'dificultad_respirar': {
+      'titulo': 'Dificultad al respirar',
+      'medicina': [
+        { 'titulo': 'Albuterol', 'precio': 10.90 },
+        { 'titulo': 'Levalbuterol', 'precio': 50.45 },
+        { 'titulo': 'Combivent', 'precio': 180 },
+      ],
+    } if dificultad_respirar == '1' else False,
+    'mucosidad': {
+      'titulo': 'Mucosidad',
+      'medicina': [
+        { 'titulo': 'Ambroxol', 'precio': 95.30 },
+        { 'titulo': 'Mucovital', 'precio': 39.50 },
+      ],
+    } if mucosidad == '1' else False,
+  }
+  return result
+
+@app.route('/')
 def index():
   return render_template('hello.html')
 
 @app.route('/prologrequest', methods=['GET', 'POST'])
 def prologrequest():
-  print('begin')
-  prol()
-  print('end')
+  data = request.get_json(force=True)
+  result = prol(data)
   return jsonify(
-    peso=123,
-    fiebre=True,
-    tos=False,
+    total_covid=result['total_covid'],
+    fiebre=result['fiebre'],
+    fatiga=result['fatiga'],
+    tos_seca=result['tos_seca'],
+    falta_apetito=result['falta_apetito'],
+    dolor_cuerpo=result['dolor_cuerpo'],
+    dificultad_respirar=result['dificultad_respirar'],
+    mucosidad=result['mucosidad'],
   )
--- static/script.js
+++ static/script.js
@@ -9,29 +9,70 @@ const tieneMucosidad = document.querySelector('#input-mucosidad');
 
 function onSubmit(e) {
   e.preventDefault();
-  console.log('evento enviado');
   const data = {
     peso: peso.value,
     fiebre: fiebre.value,
     fatiga: fatiga.value,
-    tosSeca: tosSeca.value,
-    apetito: apetito.value,
-    dolorCuerpo: dolorCuerpo.value,
-    dificultadRespira: dificultadRespira.value,
-    tieneMucosidad: tieneMucosidad.value,
+    tos_seca: tosSeca.value,
+    falta_apetito: apetito.value,
+    dolor_cuerpo: dolorCuerpo.value,
+    dificultad_respirar: dificultadRespira.value,
+    mucosidad: tieneMucosidad.value,
   };
-  console.log(data);
-  console.log('prologrequest')
   fetch('/prologrequest', {
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data), // data can be `string` or {object}!
+    method: 'POST',
+    body: JSON.stringify(data),
     headers:{
       'Content-Type': 'application/json'
     }
   })
   .then(response => response.json())
   .catch(error => console.error('Error:', error))
-  .then(response => console.log('Success:', response));
+  .then(response => {
+    const resultado = document.querySelector('.resultado')
+    const farmacias = document.querySelector('.farmacias')
+    resultado.innerHTML = '';
+    resultado.style.display = 'flex'
+    resultado.insertAdjacentHTML('afterbegin', `<h4>Usted ${response['total_covid'] > 90 ? '' : 'no'} tiene covid</h4>`)
+    resultado.insertAdjacentHTML('afterbegin', `<h3>Resultado: <span style="color:${response['total_covid'] > 90 ? 'red' : 'white'}">${response['total_covid']}%</span></h3>`)
+
+    const container = document.createElement('div')
+    container.style.display = 'flex'
+    container.style.flexWrap = 'wrap'
+    if (Object.keys(response).length > 0) {
+      resultado.insertAdjacentHTML('beforeend', `<p style="margin: 8px 0; font-size: 13px; font-weight: bold;">Usted puede tomar los siguientes medicamentos:</p>`)
+    }
+    Object.keys(response).reduce((acc, cur) => {
+      if (response[cur]) {
+        if (cur !== 'total_covid') {
+          farmacias.style.display = 'flex';
+          const div = document.createElement('div')
+          div.style.display = 'flex'
+          div.style.flexDirection = 'column'
+          div.style.width = '230px'
+          div.style.padding = '16px 0'
+          const p1 = document.createElement('p')
+          p1.style.margin = '8px 0'
+          p1.style.fontSize = '12px'
+          p1.style.fontWeight = '700'
+          const textP1 = document.createTextNode(`Para ${response[cur].titulo}:`)
+          p1.appendChild(textP1)
+          div.appendChild(p1)
+          container.appendChild(div)
+
+          response[cur].medicina.reduce((a, e) => {
+            const p2 = document.createElement('p')
+            p2.style.margin = '0 0 2px 0'
+            const textP2 = document.createTextNode(`${e.titulo} - Precio: Q. ${e.precio}`)
+            p2.appendChild(textP2)
+            div.appendChild(p2)
+            container.appendChild(div)
+          })
+        }
+      }
+    })
+    resultado.appendChild(container)
+  });
 }
 
 const submit = document.querySelector('.form');
